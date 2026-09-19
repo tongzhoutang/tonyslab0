ee2321ced9c5e8a95e0b2d165648c83eafdb2ece
RandomTony <tongzhoutang@icloud.com>
2026-09-19 15:59:44 +0800

    asked computer to draw a heart

--- main.c
+++ main.c
@@ -3,5 +3,18 @@
 int main()
 {
     // @TODO: print a sentence you want.
+    printf(" ** ** \n");
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<i;j++)
+         {
+            printf(" ");
+        }
+        for(int k=0;k<7-2*i;k++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     printf("Hello, world!\n");
 }
